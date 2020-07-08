3a08685f6e2e60a02088a904685839e904661bc4
Stiven Trujillo Ortiz <strujillo1293@gmail.com>
2020-07-07 19:06:58 -0500

    adding global css

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,9 +1,44 @@
 import React from 'react';
 import Sidebar from './Sidebar';
+import { Global, css } from '@emotion/core';
 
 const Layout = ({ children }) => {
     return ( 
         <>
+            <Global 
+                styles={css`
+                    :root {
+                        --green: #ACD038;
+                        --white: #FFF;
+                        --orange: #DA552F;
+                    }
+                    html {
+                        font-size: 62.5%;
+                        box-sizing: border-box;
+                    }
+                    *, *:before, *:after {
+                        box-sizing: inherit;
+                    }
+                    body {
+                        font-size: 1.6rem; /* ...16px  */
+                        line-height: 1.5;
+                    }
+                    h1, h2, h3 {
+                        margin: 0 0 2rem 0;
+                        line-height: 1.5;
+                    }
+                    ul {
+                        list-style: none;
+                        margin: 0;
+                        padding:0;
+                    }
+                    a {
+                        text-decoration: none;
+                    }
+
+                `}
+            />
+
             <Sidebar/>
 
             <main>
